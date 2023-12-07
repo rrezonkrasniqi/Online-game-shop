56c5b0be9ebf5aa8b8dd155303988619188d3321
Enes Paçarizi <enespaqarizi23@gmail.com>
2023-12-07 19:44:43 +0100

    Contact validate

--- ContactUs/contact.js
+++ ContactUs/contact.js
@@ -3,10 +3,26 @@ function validateAndSubmit() {
     var email = document.getElementById("email").value;
     var textField = document.getElementById("text-field").value;
 
-    if (name === "" || email === "" || textField === "") {
-        alert("Please fill in all fields.");
-    } else {
-        alert("Form submitted successfully!");
+    let nameRegex  = /[a-zA-Z]{5,10}$/;
+    let emailRegex = /[a-zA-Z0-9.-_]+@+[a-z]+\.+[a-z]{3}$/;
+    let textFieldRegex = /[a-zA-Z,.-_0-9]{5,50}/;
+
+    if(!nameRegex.test(name))
+    {
+        alert("Name input is not correct");
+        return;
+    }
+
+    if(!emailRegex.test(email))
+    {
+        alert("Email input is not correct");
+        return;
+    }
+
+    if(!textFieldRegex.test(textField))
+    {
+        alert("Text field input is not correct");
+        return;
     }
 }
 
